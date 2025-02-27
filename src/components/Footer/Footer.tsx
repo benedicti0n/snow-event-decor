ad3ad72339dd76000df498faee44517253b90035
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-28 03:34:44 +0530

    Update Footer component for improved responsiveness and text styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,16 +8,16 @@ const Footer: React.FC = () => {
             <div className='h-full flex flex-col md:flex-row justify-between items-start'>
                 {/* Logo Section */}
                 <div className="h-full flex items-center justify-center mb-6 md:mb-0 ">
-                    <Image src={"/Logos/footerLogo.png"} alt='footerLogo' height={3096} width={3096} className='h-32 w-32' />
+                    <Image src={"/Logos/footerLogo.png"} alt='footerLogo' height={3096} width={3096} className='h-20 w-20 md:h-32 md:w-32' />
                 </div>
 
                 {/* Explore Section */}
                 <div className="mb-6 md:mb-0">
                     <div className="flex items-center mb-2 gap-2">
                         <Image src={"/star.svg"} alt='icon' height={3096} width={3096} className='h-12 w-12' />
-                        <h3 className="text-4xl">Explore</h3>
+                        <h3 className="text-3xl md:text-4xl">Explore</h3>
                     </div>
-                    <ul className="space-y-1 text-xl ml-14">
+                    <ul className="space-y-1 text-base md:text-xl ml-14">
                         <li>Home</li>
                         <li>Gallery</li>
                         <li>Contact Us</li>
@@ -28,9 +28,9 @@ const Footer: React.FC = () => {
                 <div className="mb-6 md:mb-0">
                     <div className="flex items-center mb-4 gap-2">
                         <Image src={"/star.svg"} alt='icon' height={3096} width={3096} className='h-12 w-12' />
-                        <h3 className="text-4xl">Socials</h3>
+                        <h3 className="text-3xl md:text-4xl">Socials</h3>
                     </div>
-                    <ul className="space-y-1 text-xl ml-14">
+                    <ul className="space-y-1 text-base md:text-xl ml-14">
                         <li>Instagram</li>
                         <li>Facebook</li>
                     </ul>
@@ -40,9 +40,9 @@ const Footer: React.FC = () => {
                 <div>
                     <div className="flex items-center mb-4 gap-2">
                         <Image src={"/star.svg"} alt='icon' height={3096} width={3096} className='h-12 w-12' />
-                        <h3 className="text-4xl">Contacts</h3>
+                        <h3 className="text-3xl md:text-4xl">Contacts</h3>
                     </div>
-                    <ul className="space-y-1 text-xl ml-14">
+                    <ul className="space-y-1 text-base md:text-xl ml-14">
                         <li className="flex items-center">
                             <LucidePhoneOutgoing size={20} className="mr-2" />
                             <span>+1 (555) 123-45</span>
